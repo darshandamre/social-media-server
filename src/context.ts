62f979bf03029de308e08d4ec46f6c4c71c7622f
Darshan Damre <darshandamre5@gmail.com>
2022-05-12 19:52:17 +0530

    enable prisma logging

--- src/context.ts
+++ src/context.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"]
+});
 
 export type MyContext = {
   req: Request;
