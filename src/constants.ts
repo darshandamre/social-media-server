ab0f08bf0b41eac8d95a5b36e1cb448ba173e1af
Darshan Damre <darshandamre5@gmail.com>
2022-06-08 17:09:41 +0530

    update cookie options

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const COOKIE_NAME = "xid";
 export const JWT_SECRET = process.env.JWT_SECRET as string;
 export const COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
-  sameSite: "lax",
+  sameSite: "none",
   maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
   secure: true
 };
